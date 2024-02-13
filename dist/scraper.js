826d4378cfeb7724a2540624675997e920d76622
ari-abendstern <ari.abendstern@gmx.com>
2024-02-13 13:48:48 +0000

    it's giving tsc

--- dist/scraper.js
+++ dist/scraper.js
@@ -6,37 +6,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.scrapelien = void 0;
 const puppeteer_1 = __importDefault(require("puppeteer"));
 async function scrapelien() {
-    function formatRow(headers, row) {
-        if (!row) {
-            console.log('also something');
-            throw 'no row';
-        }
-        const columns = row.split("\n");
-        console.log(columns, 'columns');
-        const formattedRow = {};
-        columns.forEach((column, index) => {
-            console.log(column[index], 'individual column');
-            formattedRow.headers[index] = column;
-        });
-        return formattedRow;
-    }
     const browser = await puppeteer_1.default.launch();
     const page = await browser.newPage();
     await page.goto("https://nuforc.org/subndx/?id=all");
-    const selector = "tr";
-    await page.waitForSelector(selector);
-    const data = await page.evaluate((selector) => {
-        const elements = Array.from(document.querySelectorAll(selector));
-        return elements.map((element) => element.textContent);
-    }, selector);
+    const rows = await page.$$("tbody tr");
+    const data = await Promise.all(rows.map(async (row) => {
+        const cells = await row.$$eval("td", (tds) => tds.map((td) => td.innerText));
+        const url = await row.$$eval("td a", (links) => links[0].href);
+        return [url, ...cells];
+    }));
+    const records = data.map((row) => {
+        return {
+            link: row[0],
+            occurred: row[2],
+            city: row[3],
+            state: row[4],
+            country: row[5],
+            shape: row[6],
+            summary: row[7],
+            reported: row[8],
+            posted: row[9],
+            image: row[10] === ("" || "N") ? false : "this will be where the image url goes honey",
+        };
+    });
     await browser.close();
-    const unformattedHeaders = data.shift();
-    // console.log(data[0], 'data')
-    if (!unformattedHeaders) {
-        throw 'no headers';
-    }
-    const formattedHeaders = unformattedHeaders.match(/[A-Z][a-z]+/g);
-    console.log(formatRow(formattedHeaders, data[0]), 'thing to be returned');
-    return [];
+    console.log(`🥫🥫🕳️🪵 scraper.ts line 40 >>>>> records >>>>> `, records);
+    return records;
 }
 exports.scrapelien = scrapelien;
